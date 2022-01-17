ba58e2673b15b0e043a895e29e09607afef20495
ltty <2969846790@qq.com>
2022-01-17 23:30:09 +0800

    添加文件

---
+++ hellogit/src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author ltty
+ * @description
+ * @create 2022-01-17 23:16
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("update1");
+    }
+}
